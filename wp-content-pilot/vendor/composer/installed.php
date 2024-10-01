48909357cf84093f93686d96e46b6944347ee40c
Sean Murphy <sean@iamseanmurphy.com>
2024-10-01 01:17:34 +0000

    2.0.2

---
+++ wp-content-pilot/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pluginever/wp-content-pilot',
+        'pretty_version' => 'v2.0.2',
+        'version' => '2.0.2.0',
+        'reference' => '02ccdcb47feefdc72f4599f3fb01f99c59c7b846',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'php-curl-class/php-curl-class' => array(
+            'pretty_version' => '8.10.0',
+            'version' => '8.10.0.0',
+            'reference' => 'c97c96d5d422b4085aba32ace096c3e404dde607',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../php-curl-class/php-curl-class',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'pluginever/wp-content-pilot' => array(
+            'pretty_version' => 'v2.0.2',
+            'version' => '2.0.2.0',
+            'reference' => '02ccdcb47feefdc72f4599f3fb01f99c59c7b846',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
